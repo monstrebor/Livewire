3e1c8f76356a1d5dce76a8dca215ca461f46e1be
AramisJav <justinhereda@gmail.com>
2024-05-20 17:06:09 +0800

    add the footer

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -226,6 +226,15 @@
             </div>
             <!-- end of all about -->
 
+            <!-- footer -->
+            <footer class="w-full h-14 bg-lime-700 flex justify-center">
+                <div class="flex items-center text-2 xl font-sans cursor-pointer">
+                    <img src="img/final_logo-removebg-preview-removebg-preview.png" alt="logo" class="h-10">
+                    <p class="text-xs text-white">Privacy Notice  Copyright © 2024 Saog Cooperative. All rights reserved.</p>
+                </div>
+            </footer>
+            <!-- footer -->
+
 
     <script src="js/script.js">
     </script>
