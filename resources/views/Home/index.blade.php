e8c3a48283417206f2093e5adb91907b41ec66aa
Carl Langres <carljameslangres@gmail.com>
2024-05-21 02:21:58 +0800

    minor adjustment of photos and footer

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -205,6 +205,13 @@
             </div>
             <!-- end of all about -->
 
+            <footer class="w-full h-14 bg-lime-700 flex justify-center">
+                <div class="flex items-center text-2 xl font-sans cursor-pointer">
+                    <img src="img/final_logo-removebg-preview-removebg-preview.png" alt="logo" class="h-10">
+                    <p class="text-xs text-white">Privacy Notice  Copyright © 2024 Saog Cooperative. All rights reserved.</p>
+                </div>
+            </footer>
+
 
     <script src="js/script.js">
     </script>
