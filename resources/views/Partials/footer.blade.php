527c24dd88ba9aac9bfc3e1887a2e45b2a34cfb5
AramisJav <justinhereda@gmail.com>
2024-05-29 13:42:24 +0800

    add membership button, modify partials

--- resources/views/Partials/footer.blade.php
+++ resources/views/Partials/footer.blade.php
@@ -1,3 +1,67 @@
+<!-- all about -->
+<div class="hidden sm:block">
+    <div class="w-full h-96 bg-yellow-400 md:grid md:grid-cols-4 p-7 px-28 text-justify">
+        <div class="">
+            <p class="font-extrabold text-lime-700 text-md">Savings Products</p>
+            <p class="font-light text-lime-700 text-sm">Wallet Savings</p>
+            <p class="font-light text-lime-700 text-sm">Purposive Savings</p>
+            <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
+            <br>
+            <p class="font-extrabold text-lime-700 text-md">Loan Products</p>
+            <p class="font-light text-lime-700 text-sm">Major Loans</p>
+            <p class="font-light text-lime-700 text-sm">Minor Savings</p>
+            <p class="font-light text-lime-700 text-sm">Special Loans</p>
+            <br>
+            <p class="font-extrabold text-lime-700 text-md">Services</p>
+            <p class="font-light text-lime-700 text-sm">Wallet Savings</p>
+            <p class="font-light text-lime-700 text-sm">Purposive Savings</p>
+            <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
+            <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
+        </div>
+
+        <div class="">
+            <p class="font-extrabold text-lime-700 text-md">Membership</p>
+            <p class="font-light text-lime-700 text-sm">Wallet Savings</p>
+            <p class="font-light text-lime-700 text-sm">Purposive Savings</p>
+            <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
+            <br>
+            <p class="font-extrabold text-lime-700 text-md">Loan Products</p>
+            <p class="font-light text-lime-700 text-sm">Major Loans</p>
+            <p class="font-light text-lime-700 text-sm">Minor Savings</p>
+            <p class="font-light text-lime-700 text-sm">Special Loans</p>
+        </div>
+
+        <div class="">
+            <p class="font-extrabold text-lime-700 text-md">About Us</p>
+            <p class="font-light text-lime-700 text-sm">FAQs</p>
+            <p class="font-light text-lime-700 text-sm">Testimonials</p>
+            <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
+        </div>
+
+        <div class="">
+            <p class="font-extrabold text-lime-700 text-md">STAY IN TOUCH</p>
+            <p class="font-light text-lime-700 text-xs">Subscribe to get the latest news and updates. Get updates on our products, promos, events and more!</p>
+            <div class="relative mt-2">
+                <input type="email" placeholder="Email addres" autocomplete="email" aria-label="Email address" class="block w-full rounded-2xl border border-lime-600 bg-transparent py-4 pl-6 pr-20 text-base/6 text-neutral-950 ring-4 ring-transparent transition placeholder:text-lime-700 focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5"/>
+            </div>
+            <div class="flex justify-center">
+                <button class="cbutton mt-3 w-full">Subscribe</button>
+            </div>
+            <p class="mt-5 italic font-light text-sm text-lime-700">Contact Channels:</p>
+
+            <div class="gap-8 flex justify-center mt-3 md:gap-5">
+                <a href=""><img src="img/facebook.png" alt="FB" class="lg:w-10 lg:h-10 md:h-7 md:w-7"></a>
+                <a href=""><img src="img/instagram.png" alt="IG" class="lg:w-10 lg:h-10 md:h-7 md:w-7"></a>
+                <a href=""> <img src="img/youtube.png" alt="YT" class="lg:w-10 lg:h-10 md:h-7 md:w-7"></a>
+            </div>
+            <p class="font-light text-lime-700 text-xs mt-3">For comments/suggestions, please contact us in our official member support channels: saogcooperative@gmail.com, FB messenger</p>
+            <div class="relative mt-4">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end of all about -->
+
 <footer class="w-full h-14 bg-lime-700 flex justify-center">
     <div class="flex items-center text-2 xl font-sans cursor-pointer">
         <img src="img/final_logo-removebg-preview-removebg-preview.png" alt="logo" class="h-10">
