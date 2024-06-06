83e75b533c2051b10af09ec7339123bd20726988
AramisJav <justinhereda@gmail.com>
2024-06-06 12:07:06 +0800

    modify

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -26,7 +26,7 @@
             <div class="slideshow-container md:w-auto shadow-lg rounded-b-xl">
                 <div class="mySlides fade">
                     <div class="numbertext">1/3</div>
-                    <img src="img/coop1.jpg" class="w-full h-80 sm:h-96 object-cover">
+                    <img src="img/saog1.jpg" class="w-full h-80 sm:h-96 object-cover">
                     <div class="text">Cation Here</div>
                 </div>
                 <div class="mySlides fade">
@@ -36,7 +36,7 @@
                 </div>
                 <div class="mySlides fade">
                     <div class="numbertext">3/3</div>
-                    <img src="img/coop2.jpg" class="w-full h-80 sm:h-96 object-cover">
+                    <img src="img/saog3.jpg" class="w-full h-80 sm:h-96 object-cover">
                     <div class="text">Caption Here</div>
                 </div>
 
@@ -87,7 +87,7 @@
                     <div class="wrapper">
                         <div class="box-area">
                             <div class="box">
-                                <img src="img/coop1.jpg" alt="">
+                                <img src="img/coop1.jpg" alt="" class="object-cover">
                                 <div class="overlay">
                                     <h3>Cooperative1</h3>
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos cumque, quia eligendi porro fugit nobis.</p>
@@ -141,76 +141,12 @@
             </div>
             <!-- end of contact us -->
 
-            <!-- all about -->
-            {{-- <div class="hidden sm:block">
-                <div class="w-full h-96 bg-yellow-400 md:grid md:grid-cols-4 p-7 px-28 text-justify">
-                    <div class="">
-                        <p class="font-extrabold text-lime-700 text-md">Savings Products</p>
-                        <p class="font-light text-lime-700 text-sm">Wallet Savings</p>
-                        <p class="font-light text-lime-700 text-sm">Purposive Savings</p>
-                        <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
-                        <br>
-                        <p class="font-extrabold text-lime-700 text-md">Loan Products</p>
-                        <p class="font-light text-lime-700 text-sm">Major Loans</p>
-                        <p class="font-light text-lime-700 text-sm">Minor Savings</p>
-                        <p class="font-light text-lime-700 text-sm">Special Loans</p>
-                        <br>
-                        <p class="font-extrabold text-lime-700 text-md">Services</p>
-                        <p class="font-light text-lime-700 text-sm">Wallet Savings</p>
-                        <p class="font-light text-lime-700 text-sm">Purposive Savings</p>
-                        <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
-                        <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
-                    </div>
-
-                    <div class="">
-                        <p class="font-extrabold text-lime-700 text-md">Membership</p>
-                        <p class="font-light text-lime-700 text-sm">Wallet Savings</p>
-                        <p class="font-light text-lime-700 text-sm">Purposive Savings</p>
-                        <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
-                        <br>
-                        <p class="font-extrabold text-lime-700 text-md">Loan Products</p>
-                        <p class="font-light text-lime-700 text-sm">Major Loans</p>
-                        <p class="font-light text-lime-700 text-sm">Minor Savings</p>
-                        <p class="font-light text-lime-700 text-sm">Special Loans</p>
-                    </div>
-
-                    <div class="">
-                        <p class="font-extrabold text-lime-700 text-md">About Us</p>
-                        <p class="font-light text-lime-700 text-sm">FAQs</p>
-                        <p class="font-light text-lime-700 text-sm">Testimonials</p>
-                        <p class="font-light text-lime-700 text-sm">Long-term Savings</p>
-                    </div>
-
-                    <div class="">
-                        <p class="font-extrabold text-lime-700 text-md">STAY IN TOUCH</p>
-                        <p class="font-light text-lime-700 text-xs">Subscribe to get the latest news and updates. Get updates on our products, promos, events and more!</p>
-                        <div class="relative mt-2">
-                            <input type="email" placeholder="Email addres" autocomplete="email" aria-label="Email address" class="block w-full rounded-2xl border border-lime-600 bg-transparent py-4 pl-6 pr-20 text-base/6 text-neutral-950 ring-4 ring-transparent transition placeholder:text-lime-700 focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5"/>
-                        </div>
-                        <div class="flex justify-center">
-                            <button class="cbutton mt-3 w-full">Subscribe</button>
-                        </div>
-                        <p class="mt-5 italic font-light text-sm text-lime-700">Contact Channels:</p>
-
-                        <div class="gap-8 flex justify-center mt-3 md:gap-5">
-                            <a href=""><img src="img/facebook.png" alt="FB" class="lg:w-10 lg:h-10 md:h-7 md:w-7"></a>
-                            <a href=""><img src="img/instagram.png" alt="IG" class="lg:w-10 lg:h-10 md:h-7 md:w-7"></a>
-                            <a href=""> <img src="img/youtube.png" alt="YT" class="lg:w-10 lg:h-10 md:h-7 md:w-7"></a>
-                        </div>
-                        <p class="font-light text-lime-700 text-xs mt-3">For comments/suggestions, please contact us in our official member support channels: saogcooperative@gmail.com, FB messenger</p>
-                        <div class="relative mt-4">
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
-            <!-- end of all about -->
-
             
-            <!-- nav bar -->
+            <!-- nav footer -->
 
             @include('Partials.footer')
 
-            <!-- end of navbar -->
+            <!-- end of footer -->
 
 
     <script src="js/script.js">
