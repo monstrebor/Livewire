5b26254809b0d4ee5b38c7e67a50d287096ddc32
Carl Langres <carljameslangres@gmail.com>
2024-06-13 16:18:53 +0800

    alter membership blade

--- resources/views/Home/test.blade.php
+++ resources/views/Home/test.blade.php
@@ -1,18 +1,21 @@
+@extends('Layout.layout')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="/img/final_logo-removebg-preview-removebg-preview.png">
-    <title>Mahal na Señor Multipurpose Cooperative</title>
+
+
+
+    @section('title','Membership Register')
+
+    @section('script')
     <link rel="stylesheet" href="/css/style.css">
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
-    <style type="text/tailwindcss">
-    </style>
+    <style type="text/tailwindcss"></style>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-</head>
+    @endsection
+
+
+    @section('content')
+
 <body>
         <div>
 
@@ -551,6 +554,7 @@
     <script src="/js/dependents.js">
 
     </script>
-</body>
 
-</html>
+@endsection
+
+
