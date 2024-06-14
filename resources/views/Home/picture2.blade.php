a7302b16cb4bfe5044de651c6c24614843eccc20
Campos, Robert Janssen T <75532923+monstrebor@users.noreply.github.com>
2024-06-14 18:10:30 +0800

    set up

--- resources/views/Home/picture2.blade.php
+++ resources/views/Home/picture2.blade.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    
 
-        
+<body>
+
+
+
 
 
     @foreach ($id as $identification)
-
-    <img src="{{asset('storage/images/'.$identification->Identification_image)}}" width="100" height="100">
-                
-     @endforeach
+    <img src="{{ asset('storage/images/' . $identification->Identification_image) }}" width="100" height="100">
+    @endforeach
 
 
 
-    
-    
+
+
 
 
 </body>
+
 </html>
