702c26b8cf4663a2667480e638ab3fc270f53399
Carl Langres <carljameslangres@gmail.com>
2024-06-17 21:03:51 +0800

    fix the login route

--- resources/views/Home/login.blade.php
+++ resources/views/Home/login.blade.php
@@ -42,7 +42,7 @@
                 </div>
                 <button type="submit" class="cbutton ml-32 mt-4 rounded-2xl">Login</button>
             </form>
-            <p class="mt-4 text-gray-700 text-center">Don't have an account? <a href="{{route('regular.create')}}" class="text-lime-600 hover:underline">Register here</a>.</p>
+            <p class="mt-4 text-gray-700 text-center">Don't have an account? <a href="{{route('regular.index')}}" class="text-lime-600 hover:underline">Register here</a>.</p>
         </div>
     </div>
 
